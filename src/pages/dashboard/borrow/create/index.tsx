1325fbf7e8fc63e4bbcd9b92a658b988b7d355bc
elskow <elskow@outlook.com>
2023-12-04 21:49:21 +0700

    chore: change inter as default font

--- src/pages/dashboard/borrow/create/index.tsx
+++ src/pages/dashboard/borrow/create/index.tsx
@@ -1,12 +1,9 @@
-import { Inter } from 'next/font/google'
 import CreateBorrowForm from '@/ui/borrow/create-borrow-form'
 import Navbar from '@/ui/navbar'
 
-export const inter = Inter({ subsets: ['latin'] })
-
 export default function Page() {
 	return (
-		<div className={`${inter.className} antialiased`}>
+		<>
 			<div className='w-full flex-none p-6'>
 				<Navbar />
 			</div>
@@ -14,6 +11,6 @@ export default function Page() {
 				<h1 className='text-2xl font-semibold'>Create Borrow</h1>
 				<CreateBorrowForm />
 			</div>
-		</div>
+		</>
 	)
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,13 @@
 import Navbar from '@/ui/navbar'
-import { Inter } from 'next/font/google'
-export const inter = Inter({ subsets: ['latin'] })
 import { PrismaClient } from '@prisma/client'
+
 const prisma = new PrismaClient()
 
 import LabUsage from '@/ui/dashboard/lab-usage'
 
 export default function Page() {
 	return (
-		<div className={`${inter.className} antialiased`}>
+		<>
 			<div className='w-full flex-none p-6'>
 				<Navbar />
 			</div>
@@ -16,6 +15,6 @@ export default function Page() {
 				<h1 className='text-2xl font-semibold'>Lab Usage</h1>
 				<LabUsage />
 			</div>
-		</div>
+		</>
 	)
 }
