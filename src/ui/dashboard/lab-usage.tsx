c8c2ec1021e6ecec0621637b0cc2799b013ccb1e
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-04 15:58:35 +0700

    feature static table lab usage

---
+++ src/ui/dashboard/lab-usage.tsx
@@ -0,0 +1,19 @@
+export default function LabUsage() {
+	return (
+		<main className='p-10'>
+			<table className='max-w-md min-w-full bg-gray-200 rounded-md'>
+				<thead>
+					<tr>
+						<th>id</th>
+						<th>name</th>
+						<th>email</th>
+						<th>role</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr></tr>
+				</tbody>
+			</table>
+		</main>
+	)
+}
