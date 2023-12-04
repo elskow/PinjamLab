416ea149df74902acfc642491f89c91ecbe37681
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-04 16:24:22 +0700

    make ui of table usagelab

--- src/ui/dashboard/lab-usage.tsx
+++ src/ui/dashboard/lab-usage.tsx
@@ -2,16 +2,37 @@ export default function LabUsage() {
 	return (
 		<main className='p-10'>
 			<table className='max-w-md min-w-full bg-gray-200 rounded-md'>
-				<thead>
-					<tr>
-						<th>id</th>
-						<th>name</th>
-						<th>email</th>
-						<th>role</th>
+				<thead className=''>
+					<tr className='border-solid border-b-2 border-gray-500'>
+						<th className='border-solid border-r-2 border-gray-500'>
+							id
+						</th>
+						<th className='border-solid border-x-2 border-gray-500'>
+							name
+						</th>
+						<th className='border-solid border-x-2 border-gray-500'>
+							email
+						</th>
+						<th className='border-solid border-l-2 border-gray-500'>
+							role
+						</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr></tr>
+					<tr>
+						<td className='border-solid border-r-2 border-gray-500'>
+							1
+						</td>
+						<td className='border-solid border-x-2 border-gray-500'>
+							2
+						</td>
+						<td className='border-solid border-x-2 border-gray-500'>
+							3
+						</td>
+						<td className='border-solid border-l-2 border-gray-500'>
+							4
+						</td>
+					</tr>
 				</tbody>
 			</table>
 		</main>
