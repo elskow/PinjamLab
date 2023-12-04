a5b2597e075dfcb206ce289b35251c6e891c2ee9
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-04 15:59:03 +0700

    add LabUsage component to dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,10 @@
 import Navbar from '@/ui/navbar'
 import { Inter } from 'next/font/google'
-
 export const inter = Inter({ subsets: ['latin'] })
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+
+import LabUsage from '@/ui/dashboard/lab-usage'
 
 export default function Page() {
 	return (
@@ -11,23 +12,9 @@ export default function Page() {
 			<div className='w-full flex-none p-6'>
 				<Navbar />
 			</div>
-			<div className='flex-grow p-6 md:overflow-y-auto md:p-12 bg-gray-200'>
-				<table>
-					<thead>
-						<tr>
-							<th>id</th>
-							<th>name</th>
-							<th>email</th>
-							<th>role</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>John Doe</td>
-						</tr>
-					</tbody>
-				</table>
+			<div className='text-center'>
+				<h1 className='text-2xl font-semibold'>Lab Usage</h1>
+				<LabUsage />
 			</div>
 		</div>
 	)
