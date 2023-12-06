3f7f8ccf13bc2d367ce544287c31dbec11e31e3f
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-06 13:21:24 +0700

    aply dashboard layout to dashboard and create page

--- src/pages/dashboard/create/index.tsx
+++ src/pages/dashboard/create/index.tsx
@@ -1,16 +1,13 @@
 import CreateBorrowForm from '@/ui/borrow/create-borrow-form'
-import Navbar from '@/ui/navbar'
+import Layout from '@/pages/dashboard/DashboardLayout'
 
 export default function Page() {
 	return (
-		<>
-			<div className='w-full flex-none p-6'>
-				<Navbar />
-			</div>
+		<Layout>
 			<div className='text-center'>
 				<h1 className='text-2xl font-semibold'>Create Borrow</h1>
 				<CreateBorrowForm />
 			</div>
-		</>
+		</Layout>
 	)
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,81 +1,19 @@
-import Navbar from '@/ui/navbar'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import Layout from './DashboardLayout'
 
 import LabUsage from '@/ui/dashboard/lab-usage'
-import Link from 'next/link'
-import { FaHome, FaUser, FaBook, FaNewspaper, FaChartBar } from 'react-icons/fa'
-
-const navItems = [
-	{
-		label: 'Home',
-		icon: <FaHome />,
-		href: '/',
-	},
-	{
-		label: 'Akun',
-		icon: <FaUser />,
-		href: '/',
-	},
-	{
-		label: 'Peminjaman',
-		icon: <FaBook />,
-		href: '/dashboard',
-	},
-	{
-		label: 'Berita Acara',
-		icon: <FaNewspaper />,
-		href: '/',
-	},
-	{
-		label: 'Lab dalam Angka',
-		icon: <FaChartBar />,
-		href: '/',
-	},
-]
-
-interface NavItemProps {
-	item: {
-		label: string
-		icon: JSX.Element
-		href: string
-	}
-}
-
-const NavItem: React.FC<NavItemProps> = ({ item }) => (
-	<li key={item.label}>
-		<Link
-			className='flex items-center space-x-2 p-2 text-slate-800 hover:bg-blue-500 hover:text-white rounded transition-colors'
-			href={item.href}
-		>
-			{item.icon}
-			<span>{item.label}</span>
-		</Link>
-	</li>
-)
 
 export default function Page() {
 	return (
-		<div className='min-h-screen bg-gray-100 flex flex-col'>
-			<nav className='bg-blue-800 p-4 flex justify-between items-center shadow-md sticky top-0 z-50'>
-				<div className='text-white text-lg font-bold'>PinjamLab</div>
-			</nav>
-			<div className='flex flex-grow overflow-y-hidden'>
-				<aside className='w-full md:w-64 bg-white p-4 border-r border-gray-200 pt-12 md:pt-6 md:sticky md:top-0 h-[calc(100vh-4rem)] overflow-y-auto'>
-					<nav>
-						<ul className='space-y-2 text-sm'>
-							{navItems.map((item) => (
-								<NavItem item={item} />
-							))}
-						</ul>
-					</nav>
-				</aside>
-				<main className='flex-grow p-8'>
-					<h1>Lab Usage</h1>
-					<LabUsage />
-				</main>
+		<Layout>
+			<div>
+				<h1 className='text-2xl font-semibold px-10'>Lab Usage</h1>
+				<input
+					type='text'
+					placeholder='Search'
+					className='border-2 border-gray-300 rounded-md p-2 mx-10'
+				/>
+				<LabUsage />
 			</div>
-		</div>
+		</Layout>
 	)
 }
