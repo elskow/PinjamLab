f7126907f155d52c5633635cd4f51b6302394364
elskow <elskow@outlook.com>
2023-12-08 00:05:54 +0700

    ui: make the scheduleitems more popup

--- src/ui/components/schedule/ScheduleItems.tsx
+++ src/ui/components/schedule/ScheduleItems.tsx
@@ -11,18 +11,18 @@ interface ScheduleProps {
 
 export default function ScheduleItems({ day, schedules }: ScheduleProps) {
 	return (
-		<div className='p-6 bg-white rounded-lg shadow space-y-2'>
-			<div className='font-bold text-slate-900 text-left text-lg mb-2'>
+		<div className='p-6 bg-white rounded-lg shadow-lg space-y-4'>
+			<div className='font-bold text-lg text-left text-gray-900 mb-2'>
 				{day}
 			</div>
 			{schedules.map((schedule) => (
 				<div
 					key={schedule.id}
-					className={`p-4 bg-white rounded-lg shadow space-y-2 ${
+					className={`p-4 bg-gray-100 rounded-lg shadow-lg space-y-2 ${
 						schedule.day === day ? `` : `hidden`
 					}`}
 				>
-					<h5 className='font-semibold text-gray-900'>
+					<h5 className='font-semibold text-md text-gray-900'>
 						{schedule.matakuliah}
 					</h5>
 					<p className='text-sm text-gray-600'>{schedule.dosen}</p>
