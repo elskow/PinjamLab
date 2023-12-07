132aee8099445c211960ab6f3be91d078c67b56a
NaufalF121 <60810604+NaufalF121@users.noreply.github.com>
2023-12-08 01:26:07 +0700

    fix hover news

--- src/ui/components/news/NewsItem.tsx
+++ src/ui/components/news/NewsItem.tsx
@@ -11,11 +11,11 @@ interface NewsItemProps {
 
 const NewsItem = ({ title, date, description, image, href }: NewsItemProps) => {
 	return (
-		<div className='flex flex-col mb-6 bg-gray-100 hover:shadow-lg rounded-lg transition-all duration-200 ease-in-out'>
+		<div className='flex flex-col mb-6 bg-gray-100 rounded-lg transition-all duration-200 ease-in-out'>
 			<p className='font-semibold text-gray-600 text-left text-sm p-6'>
 				{date}
 			</p>
-			<div className='flex flex-row'>
+			<div className='flex flex-row hover:shadow-lg'>
 				<Link
 					href={href}
 					className='flex shadow-md hover:shadow-md flex-row rounded flex-grow gap-2 p-6 bg-white space-x-4 items-center'
