70fb20921001edb73545453c7599047f1cf22b7f
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-07 13:19:34 +0700

    make landing for schedule page

--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -1,5 +1,26 @@
 import Home from '@/ui/MainLayout'
 
+const dummyDataSchedule = [
+	{
+		id: 1,
+		matakuliah: 'Pemrograman Berbasis Platform',
+		dosen: 'Faris Darari',
+		waktu: '09.00 - 11.00',
+	},
+	{
+		id: 2,
+		matakuliah: 'DDP 2',
+		dosen: 'Kukuh Nugroho',
+		waktu: '13.00 - 15.00',
+	},
+	{
+		id: 3,
+		matakuliah: 'Pemrosesan Citra Digital',
+		dosen: 'Arief Kurniawan',
+		waktu: '15.00 - 17.00',
+	},
+]
+
 export default function Page() {
 	return (
 		<Home>
@@ -7,6 +28,44 @@ export default function Page() {
 				<h1 className='text-2xl font-semibold px-10'>
 					Jadwal Penggunaan Kelas
 				</h1>
+				{/*Md screen */}
+				<div className='md:grid hidden grid-cols-3 space-x-4 m-10'>
+					{dummyDataSchedule.map((schedule) => (
+						<div
+							key={schedule.id}
+							className='p-4 bg-white rounded-lg shadow space-y-2'
+						>
+							<h5 className='font-bold text-gray-900'>
+								{schedule.matakuliah}
+							</h5>
+							<p className='text-sm text-gray-600'>
+								{schedule.dosen}
+							</p>
+							<p className='text-sm text-gray-600'>
+								{schedule.waktu}
+							</p>
+						</div>
+					))}
+				</div>
+				{/* !md screen */}
+				<div className='md:hidden space-y-4 m-10'>
+					{dummyDataSchedule.map((schedule) => (
+						<div
+							key={schedule.id}
+							className='p-4 bg-white rounded-lg shadow space-y-2'
+						>
+							<h5 className='font-bold text-gray-900'>
+								{schedule.matakuliah}
+							</h5>
+							<p className='text-sm text-gray-600'>
+								{schedule.dosen}
+							</p>
+							<p className='text-sm text-gray-600'>
+								{schedule.waktu}
+							</p>
+						</div>
+					))}
+				</div>
 			</div>
 		</Home>
 	)
