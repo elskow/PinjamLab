e4731f58325cca3b200eae36484c4ce5323e2a28
NaufalF121 <60810604+NaufalF121@users.noreply.github.com>
2023-12-08 16:29:49 +0700

    add: init about us page

---
+++ src/pages/aboutus/index.tsx
@@ -0,0 +1,14 @@
+import MainLayout from '@/ui/MainLayout'
+
+
+export default function Page() {
+	return (
+		<MainLayout>
+			<div className='md:px-10 m-4'>
+				<h1 className='text-2xl font-semibold mb-6 text-gray-900'>
+					About Us
+				</h1>
+			</div>
+		</MainLayout>
+	)
+}
