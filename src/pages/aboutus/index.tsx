aaebb73c1bf0bad5175716966cf5e585f1e1122b
NaufalF121 <NaufalF121@users.noreply.github.com>
2023-12-08 10:22:41 +0000

    Apply formatting changes

--- src/pages/aboutus/index.tsx
+++ src/pages/aboutus/index.tsx
@@ -10,12 +10,20 @@ export default function Page() {
 			</div>
 			<div className='flex flex-col mb-4 p-10 space-y-10'>
 				<div className='flex flex-col gap-2 items-center justify-center'>
-					<h1 className='text-3xl font-bold text-center text-black'>Meet Our Team </h1>
+					<h1 className='text-3xl font-bold text-center text-black'>
+						Meet Our Team{' '}
+					</h1>
 				</div>
 				<div className='flex flex-row gap-2 items-center'>
-					<div className='basis-1/3 rounded bg-white shadow-lg'>1</div>
-					<div className='basis-1/3 rounded bg-white shadow-lg'>2</div>
-					<div className='basis-1/3 rounded bg-white shadow-lg'>3</div>
+					<div className='basis-1/3 rounded bg-white shadow-lg'>
+						1
+					</div>
+					<div className='basis-1/3 rounded bg-white shadow-lg'>
+						2
+					</div>
+					<div className='basis-1/3 rounded bg-white shadow-lg'>
+						3
+					</div>
 				</div>
 			</div>
 		</MainLayout>
