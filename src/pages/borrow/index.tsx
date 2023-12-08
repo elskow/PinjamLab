34e6388b50dfd88acc842f6d617473ff51154f1c
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-08 14:22:47 +0700

    fix: responsive margin

--- src/pages/borrow/index.tsx
+++ src/pages/borrow/index.tsx
@@ -5,13 +5,13 @@ import LabUsage from '@/ui/components/borrow/lab-usage'
 export default function Page() {
 	return (
 		<MainLayout>
-			<div className='px-10 m-4'>
+			<div className='m-4 text-gray-900'>
 				<h1 className='text-2xl font-semibold mb-6'>Lab Usage</h1>
 				<div className='flex items-center justify-between bg-white p-3 shadow-sm rounded-lg mb-6'>
 					<input
 						type='text'
 						placeholder='Search...'
-						className='flex-grow outline-none px-4'
+						className='flex-grow outline-none px-4 bg-white text-gray-900'
 					/>
 					<Create />
 				</div>
