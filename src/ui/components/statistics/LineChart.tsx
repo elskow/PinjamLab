64df52343aed8e4de7dcb892554e2f4247b373ec
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-08 12:49:43 +0700

    fix: linechart

--- src/ui/components/statistics/LineChart.tsx
+++ src/ui/components/statistics/LineChart.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Line } from 'react-chartjs-2'
 import {
 	Chart as ChartjS,
@@ -24,60 +24,29 @@ ChartjS.register(
 )
 
 export default function LineChart() {
-	const [chartData, setChartData] = useState({
-		datasets: [],
+	const [chartData] = useState({
+		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+		datasets: [
+			{
+				label: 'Acepted',
+				data: [10, 5, 8, 8, 9, 10],
+				fill: false,
+				backgroundColor: 'rgb(255, 99, 132)',
+				borderColor: 'rgba(255, 99, 132)',
+			},
+			{
+				label: 'Requested',
+				data: [12, 10, 9, 10, 12, 15],
+				fill: false,
+				borderColor: 'rgb(54, 162, 235)',
+				backgroundColor: 'rgb(54, 162, 235)',
+			},
+		],
 	})
 
-	const [chartOptions, setChartOptions] = useState({})
-
-	useEffect(() => {
-		const data = {
-			labels: [
-				'Jan',
-				'Feb',
-				'Mar',
-				'Apr',
-				'May',
-				'Jun',
-				'Jul',
-				'Aug',
-				'Sept',
-				'Nov',
-				'Dec',
-			],
-			usePointStyle: true,
-			datasets: [
-				{
-					label: 'Acepted',
-					data: [9, 11, 3, 5, 11, 3, 8, 1, 3, 5, 2, 3],
-					backgroundColor: 'rgba(255, 99, 132, 0.2)',
-					borderColor: 'rgba(255, 99, 132, 1)',
-					borderWidth: 1,
-				},
-				{
-					label: 'Request',
-					data: [2, 3, 11, 6, 15, 4, 2, 3, 15, 5, 1],
-					backgroundColor: 'rgba(54, 162, 235, 0.2)',
-					borderColor: 'rgba(54, 162, 235, 1)',
-					borderWidth: 1,
-				},
-			],
-		}
-
-		const options = {
-			scales: {
-				y: {
-					beginAtZero: true,
-				},
-			},
-		}
-
-		setChartData(data)
-		setChartOptions(options)
-	}, [])
 	return (
 		<div>
-			<Line data={chartData} options={chartOptions} />
+			<Line data={chartData} />
 		</div>
 	)
 }
