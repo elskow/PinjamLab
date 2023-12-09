32348d84bb177835bcdee46cdf6fa8247e855f55
NaufalF121 <NaufalF121@users.noreply.github.com>
2023-12-09 05:24:55 +0000

    Apply formatting changes

--- src/ui/components/news/NewsGroupItem.tsx
+++ src/ui/components/news/NewsGroupItem.tsx
@@ -12,29 +12,29 @@ interface NewsItemProps {
 const NewsItem = ({ title, description, image, href }: NewsItemProps) => {
 	return (
 		<div className='flex flex-row hover:shadow-lg'>
-		<Link
-			href={href}
-			className='flex shadow-md sm:flex-row flex-col rounded-md flex-grow gap-2 p-6 bg-white space-x-4 items-center'
-		>
-			<div className='flex flex-shrink-0'>
-				<Image
-					className='rounded-lg sm:w-32 sm:h-32'
-					alt='gambar'
-					width='200'
-					height='120'
-					src={image}
-				/>
-			</div>
-			<div className='flex flex-col'>
-				<h5 className='font-bold text-gray-900 text-left text-lg'>
-					{title}
-				</h5>
-				<p className='text-sm text-gray-600 text-left'>
-					{description}
-				</p>
-			</div>
-		</Link>
-	</div>
+			<Link
+				href={href}
+				className='flex shadow-md sm:flex-row flex-col rounded-md flex-grow gap-2 p-6 bg-white space-x-4 items-center'
+			>
+				<div className='flex flex-shrink-0'>
+					<Image
+						className='rounded-lg sm:w-32 sm:h-32'
+						alt='gambar'
+						width='200'
+						height='120'
+						src={image}
+					/>
+				</div>
+				<div className='flex flex-col'>
+					<h5 className='font-bold text-gray-900 text-left text-lg'>
+						{title}
+					</h5>
+					<p className='text-sm text-gray-600 text-left'>
+						{description}
+					</p>
+				</div>
+			</Link>
+		</div>
 	)
 }
 export default NewsItem
