ae038282a84c72b8d0270162b77862b5ecdaba9e
elskow <elskow@users.noreply.github.com>
2023-12-09 15:47:51 +0000

    Apply formatting changes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,22 +3,22 @@ import NextAuth from 'next-auth/next'
 import Google from 'next-auth/providers/google'
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-    throw new Error('The Google client ID and secret must be defined')
+	throw new Error('The Google client ID and secret must be defined')
 }
 
 export const authOptions = {
-    providers: [
-        Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            authorization: {
-                params: {
-                    hd: 'unesa.ac.id',
-                },
-            },
-        }),
-    ],
+	providers: [
+		Google({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			authorization: {
+				params: {
+					hd: 'unesa.ac.id',
+				},
+			},
+		}),
+	],
 }
 
 export default (req: NextApiRequest, res: NextApiResponse): Promise<void> =>
-    NextAuth(req, res, authOptions) as Promise<void>
+	NextAuth(req, res, authOptions) as Promise<void>
