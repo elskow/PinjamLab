aba8aefe1470421daf07a39c02ccb34a6928c501
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-09 12:19:40 +0700

    fix: responsive card

--- src/pages/aboutus/index.tsx
+++ src/pages/aboutus/index.tsx
@@ -1,6 +1,27 @@
 import MainLayout from '@/ui/MainLayout'
 import Image from 'next/image'
 
+const dataTeam = [
+	{
+		id: 1,
+		nama: 'Naufal Farras Pratama',
+		src: 'https://avatars.githubusercontent.com/u/60810604?',
+		quote: 'All in Anies no 1',
+	},
+	{
+		id: 2,
+		nama: 'Helmy LuqmanulHakim',
+		src: 'https://avatars.githubusercontent.com/u/103118501',
+		quote: 'Coblos no 2',
+	},
+	{
+		id: 3,
+		nama: 'Ahmd Mufahras Li Assardew',
+		src: 'https://avatars.githubusercontent.com/u/117156042',
+		quote: 'Coblos no 9',
+	},
+]
+
 export default function Page() {
 	return (
 		<MainLayout>
@@ -15,52 +36,27 @@ export default function Page() {
 						Meet Our Team{' '}
 					</h1>
 				</div>
-				<div className='flex flex-row gap-4 items-center'>
-					<div className='flex flex-col basis-1/3 rounded-xl bg-white shadow-lg p-8 space-y-4'>
-						<Image
-							className='rounded-full self-center'
-							width='170'
-							height='170'
-							alt='gambar'
-							src='https://avatars.githubusercontent.com/u/60810604?'
-						/>
-						<div className='flex flex-col items-center gap-2'>
-							<h1 className='text-bold text-black'>
-								Naufal Farras Pratama
-							</h1>
-							<p>"All in Anies no 1"</p>
+				<div className='md:flex md:flex-row grid grid-cols-2  gap-4 items-center'>
+					{dataTeam.map((team, index) => (
+						<div
+							key={index}
+							className='flex flex-col basis-1/3 rounded-xl bg-white shadow-lg p-8 space-y-4'
+						>
+							<Image
+								className='rounded-full self-center'
+								width='170'
+								height='170'
+								alt='gambar'
+								src={team.src}
+							/>
+							<div className='flex flex-col items-center gap-2'>
+								<h1 className='text-bold text-black'>
+									{team.nama}
+								</h1>
+								<p>{team.quote}</p>
+							</div>
 						</div>
-					</div>
-					<div className='flex flex-col basis-1/3 rounded-xl bg-white shadow-lg p-8 space-y-4'>
-						<Image
-							className='rounded-full self-center'
-							width='170'
-							height='170'
-							alt='gambar'
-							src='https://avatars.githubusercontent.com/u/103118501'
-						/>
-						<div className='flex flex-col items-center gap-2'>
-							<h1 className='text-bold text-black'>
-								Helmy LuqmanulHakim
-							</h1>
-							<p>"Coblos no 2"</p>
-						</div>
-					</div>
-					<div className='flex flex-col basis-1/3 rounded-xl bg-white shadow-lg p-8 space-y-4'>
-						<Image
-							className='rounded-full self-center'
-							width='170'
-							height='170'
-							alt='gambar'
-							src='https://avatars.githubusercontent.com/u/117156042'
-						/>
-						<div className='flex flex-col items-center gap-2'>
-							<h1 className='text-bold text-black'>
-								Ahmd Mufahras Li Assardew
-							</h1>
-							<p>"Coblos no 3"</p>
-						</div>
-					</div>
+					))}
 				</div>
 			</div>
 		</MainLayout>
