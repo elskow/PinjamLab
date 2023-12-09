bcec032ef5c122af1345af1d5b5bad3016331d50
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-09 22:23:48 +0700

    fix: responsive mobile

--- src/pages/aboutus/index.tsx
+++ src/pages/aboutus/index.tsx
@@ -1,4 +1,5 @@
 import MainLayout from '@/ui/MainLayout'
+import Link from 'next/link'
 import Image from 'next/image'
 
 const dataTeam = [
@@ -7,18 +8,21 @@ const dataTeam = [
 		nama: 'Naufal Farras Pratama',
 		src: 'https://avatars.githubusercontent.com/u/60810604?',
 		quote: 'All in Anies no 1',
+		link: 'https://github.com/NaufalF121',
 	},
 	{
 		id: 2,
 		nama: 'Helmy LuqmanulHakim',
 		src: 'https://avatars.githubusercontent.com/u/103118501',
 		quote: 'Coblos no 2',
+		link: 'https://github.com/elskow',
 	},
 	{
 		id: 3,
 		nama: 'Ahmd Mufahras Li Assardew',
 		src: 'https://avatars.githubusercontent.com/u/117156042',
 		quote: 'Coblos no 9',
+		link: 'https://github.com/alfazh123',
 	},
 ]
 
@@ -36,26 +40,28 @@ export default function Page() {
 						Meet Our Team{' '}
 					</h1>
 				</div>
-				<div className='md:flex md:flex-row grid grid-cols-2  gap-4 items-center'>
+				<div className='md:flex md:flex-row grid grid-cols-1  gap-4 items-center'>
 					{dataTeam.map((team, index) => (
-						<div
-							key={index}
-							className='flex flex-col basis-1/3 rounded-xl bg-white shadow-lg p-8 space-y-4'
-						>
-							<Image
-								className='rounded-full self-center'
-								width='170'
-								height='170'
-								alt='gambar'
-								src={team.src}
-							/>
-							<div className='flex flex-col items-center gap-2'>
-								<h1 className='text-bold text-black'>
-									{team.nama}
-								</h1>
-								<p>{team.quote}</p>
+						<Link key={index} href={team.link}>
+							<div
+								key={index}
+								className='flex flex-col basis-1/3 rounded-xl bg-white shadow-lg p-8 space-y-4'
+							>
+								<Image
+									className='rounded-full self-center'
+									width='170'
+									height='170'
+									alt='gambar'
+									src={team.src}
+								/>
+								<div className='flex flex-col items-center gap-2'>
+									<h1 className='text-bold text-black'>
+										{team.nama}
+									</h1>
+									<p>{team.quote}</p>
+								</div>
 							</div>
-						</div>
+						</Link>
 					))}
 				</div>
 			</div>
