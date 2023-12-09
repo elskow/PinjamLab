294675d27b57c5d742798418efef3d441a6bc14c
elskow <elskow@outlook.com>
2023-12-09 23:55:07 +0700

    ui: its already has header, why keeping it?

--- src/pages/aboutus/index.tsx
+++ src/pages/aboutus/index.tsx
@@ -29,11 +29,6 @@ const dataTeam = [
 export default function Page() {
 	return (
 		<MainLayout>
-			<div className='md:px-10 m-4'>
-				<h1 className='text-2xl font-semibold mb-6 text-gray-900'>
-					About Us
-				</h1>
-			</div>
 			<div className='flex flex-col mb-4 p-10 space-y-10'>
 				<div className='flex flex-col gap-2 items-center justify-center'>
 					<h1 className='text-3xl font-bold text-center text-black'>
