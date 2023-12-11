99935a1f56e1333078fe15a10fd8fab6857b8318
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-12-11 16:37:32 +0700

    fix: baground and font

--- src/ui/components/schedule/ScheduleGroupItems.tsx
+++ src/ui/components/schedule/ScheduleGroupItems.tsx
@@ -3,7 +3,6 @@ interface ScheduleItemsProps {
 	matakuliah: string
 	dosen: string
 	waktu: string
-	day: string
 }
 
 export default function ScheduleGroupItems({
@@ -11,48 +10,39 @@ export default function ScheduleGroupItems({
 	matakuliah,
 	dosen,
 	waktu,
-	day,
 }: ScheduleItemsProps) {
 	return (
-		<button>
-			<div className='flex flex-row'>
-				<button
-					onClick={() =>
-						(
-							document.getElementById(
-								id.toString(),
-							) as HTMLDialogElement
-						).showModal()
-					}
-					className='= flex flex-grow flex-col items-center gap-2 space-x-4 rounded-md sm:flex-row'
-				>
-					<div className='flex flex-col'>
-						<h5 className='text-left text-lg font-bold text-gray-900'>
-							{matakuliah}
-						</h5>
-						<p className='text-left text-sm text-gray-600'>
-							{dosen}
-						</p>
-						<p className='text-left text-sm text-gray-600'>
-							{waktu}
-						</p>
-						<p className='text-left text-sm text-gray-600'>{day}</p>
-					</div>
-				</button>
-				<dialog id={id.toString()} className='glass modal'>
-					<div className='modal-box w-11/12 max-w-5xl bg-white'>
-						<h3 className='text-lg font-bold text-gray-900'>
-							{matakuliah}
-						</h3>
-						<p className='py-4 text-sm text-gray-600'>{dosen}</p>
-						<p className='py-4 text-sm text-gray-600'>{waktu}</p>
-						<p className='py-4 text-sm text-gray-600'>{day}</p>
-					</div>
-					<form method='dialog' className='modal-backdrop'>
-						<button>close</button>
-					</form>
-				</dialog>
-			</div>
-		</button>
+		<div className='flex flex-row rounded-md bg-gray-100 shadow-md hover:shadow-xl'>
+			<button
+				onClick={() =>
+					(
+						document.getElementById(
+							id.toString(),
+						) as HTMLDialogElement
+					).showModal()
+				}
+				className='= flex flex-grow flex-col items-center gap-2 space-x-4 rounded-md p-6 sm:flex-row'
+			>
+				<div className='flex flex-col'>
+					<h5 className='text-md text-left font-semibold text-gray-900'>
+						{matakuliah}
+					</h5>
+					<p className='text-left text-sm text-gray-600'>{dosen}</p>
+					<p className='text-left text-sm text-gray-600'>{waktu}</p>
+				</div>
+			</button>
+			<dialog id={id.toString()} className='glass modal'>
+				<div className='modal-box w-11/12 max-w-5xl bg-white'>
+					<h3 className='text-lg font-bold text-gray-900'>
+						{matakuliah}
+					</h3>
+					<p className='text-sm text-gray-600'>{dosen}</p>
+					<p className='text-sm text-gray-600'>{waktu}</p>
+				</div>
+				<form method='dialog' className='modal-backdrop'>
+					<button>close</button>
+				</form>
+			</dialog>
+		</div>
 	)
 }
