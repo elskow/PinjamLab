eb9e939fe65d25b667db1db32deda98773171747
elskow <elskow@outlook.com>
2023-12-21 22:30:37 +0700

    fix: error on prod

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -38,7 +38,8 @@ export const api = createTRPCNext<AppRouter>({
 					enabled: (opts) =>
 						process.env.NODE_ENV === 'development' ||
 						(opts.direction === 'down' &&
-							opts.result instanceof Error),
+							opts.result instanceof Error) ||
+						process.env.NODE_ENV !== 'production',
 				}),
 				httpBatchLink({
 					url: `${getBaseUrl()}/api/trpc`,
