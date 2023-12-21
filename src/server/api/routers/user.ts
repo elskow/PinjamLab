c8c9703fc126d49352ed558a33046670e61a4e12
elskow <elskow@users.noreply.github.com>
2023-12-21 18:32:24 +0000

    Apply formatting changes

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -4,46 +4,42 @@ import { createTRPCRouter, publicProcedure } from '@/server/api/trpc'
 import { db } from '@/server/db'
 
 export const userRouter = createTRPCRouter({
-    addUser: publicProcedure
-        .input(z.string())
-        .mutation(async ({ input }) => {
-            const email = input.trim();
-            const user = await db.user.findFirst({
-                where: { email },
-            })
+	addUser: publicProcedure.input(z.string()).mutation(async ({ input }) => {
+		const email = input.trim()
+		const user = await db.user.findFirst({
+			where: { email },
+		})
 
-            if (!user) {
-				try{
-					await db.user.create({
-						data: {
-							email,
-							role: 'User',
-						},
-					})
-				} catch (e) {
-					// do nothing
-				}
-            }
+		if (!user) {
+			try {
+				await db.user.create({
+					data: {
+						email,
+						role: 'User',
+					},
+				})
+			} catch (e) {
+				// do nothing
+			}
+		}
 
-            return {
-                user,
-            }
-        }),
+		return {
+			user,
+		}
+	}),
 
-    getRole: publicProcedure
-        .input(z.string())
-        .query(async ({ input }) => {
-            const email = input.trim();
-            const user = await db.user.findFirst({
-                where: { email },
-            })
+	getRole: publicProcedure.input(z.string()).query(async ({ input }) => {
+		const email = input.trim()
+		const user = await db.user.findFirst({
+			where: { email },
+		})
 
-            if (!user) {
-                throw new Error('User not found')
-            }
+		if (!user) {
+			throw new Error('User not found')
+		}
 
-            return {
-                role: user.role,
-            }
-        }),
+		return {
+			role: user.role,
+		}
+	}),
 })
